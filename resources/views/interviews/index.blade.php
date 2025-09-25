3577145828723697592a669407bb71444d2948ef
cast <cdston@gmail.com>
2025-09-25 09:49:34 -0300

    correção remover filtro, ajuste responsivo curriculo externo

--- resources/views/interviews/index.blade.php
+++ resources/views/interviews/index.blade.php
@@ -726,7 +726,8 @@ function updateActiveFilters(){
         if( key !== 'page' && value && value !== 'Todos' && value !== 'Todas'){
             // Para arrays (selects múltiplos)
             if (key.endsWith('[]')){
-                activeFilters.push(createFilterBadge(key.replace('[]', ''), value));
+                //activeFilters.push(createFilterBadge(key.replace('[]', ''), value));
+                activeFilters.push(createFilterBadge(key, value));
             } else {
                 activeFilters.push(createFilterBadge(key, value));
             }
