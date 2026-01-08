730f456774cd85226dfbe26c57e482858ac147b3
cast <cdston@gmail.com>
2026-01-08 10:36:57 -0300

    logo

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -61,7 +61,9 @@
                         @if (Auth::user()->avatar != null)
                             <img src="{{ Auth::user()->avatar }}" class="img-circle elevation-2" alt="User Image">
                         @else
-                            <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                            {{-- <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                                alt="User Image"> --}}
+                            <img src="{{ asset('admin/dist/img/ekklesia-favicon.png') }}" class="img-circle elevation-2"
                                 alt="User Image">
                         @endif
                     </div>
